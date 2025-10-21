41b6312ef240e449b9e4f4648c7d216fb823321b
Tiago dos Santos <tiago2904santos@gmail.com>
2025-10-21 16:10:24 -0300

    adicionando funcionalidades com js

---
+++ js/app.js
@@ -0,0 +1,46 @@
+let totalGeral;
+limpar();
+
+function adicionar() {
+    //recuperar valores: nome do produto, quantidade e valor.
+
+    let produto = document.getElementById("produto").value;
+    let nomeProduto = produto.split("-")[0];
+    let valorUnitario = produto.split("R$")[1];
+    let quantidade = document.getElementById("quantidade").value;
+    if (quantidade == 0) {
+        alert("Informe a quantidade de produtos!")
+        return;
+    }
+
+    //calcular o preço, o nosso subtotal.
+    
+    let preco = quantidade * valorUnitario;
+
+    
+    //adicionar no carrinho.
+
+    let carrinho = document.getElementById("lista-produtos");
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`
+
+
+    //atualizar o valor total da compra.
+
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById("valor-total");
+    campoTotal.textContent = `R$${totalGeral}`
+    document.getElementById("quantidade").value = 0;
+    
+
+
+}
+
+
+
+function limpar() {
+    totalGeral = 0
+    document.getElementById("lista-produtos").innerHTML = "";
+    document.getElementById("valor-total").textContent ="R$ 0,00";
+}
